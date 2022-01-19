bc5b3072f1b51e67842192dc284e7fab37e80398
ZP <zairaperez01@gmail.com>
2022-01-19 08:29:26 -0600

    fetch call

---
+++ assets/js/videoscript.js
@@ -0,0 +1,17 @@
+
+var getYoutubeHeadlines = function() {
+    var topYoutubeUrl = "" + apiKey + "&lang=en&country=us&max=10";
+    fetch(topYoutubeUrl).then(function(response){
+        if(response.ok){         
+                response.json().then(function(data){
+                displayresultSubtitleEl.textContent = "Top 10 US Headlines:";
+                displayData(data);
+            })
+        }
+        else
+            alert("APi does not work");
+    })
+    .catch(function(error){
+        alert("something went wrong: looks like news api is down");
+    })
+}
