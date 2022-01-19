126110bb0a0001595f903f31f35a9ff255dc01ae
ZP <zairaperez01@gmail.com>
2022-01-19 08:52:12 -0600

    fetch call by keyword

--- assets/js/videoscript.js
+++ assets/js/videoscript.js
@@ -15,3 +15,20 @@ var getYoutubeHeadlines = function() {
         alert("something went wrong: looks like news api is down");
     })
 }
+
+//get news based on url
+var getNewsByKeyword = function(url){
+    fetch(url).then(function(response){
+        if(response.ok){       
+            response.json().then(function(data){
+                console.log(data);
+                displayData(data);
+            })
+        }
+        else
+            alert("APi does not work");
+    })
+    .catch(function(error){
+        alert("something went wrong: looks like news api is down");
+    })
+}
